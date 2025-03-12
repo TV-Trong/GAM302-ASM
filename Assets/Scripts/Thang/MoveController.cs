a0ca58b19ba8cb0937615a1034db12135a2f94f2
bapcao <caorua04@gmail.com>
2025-03-12 16:00:32 +0700

    move script test
    thử nghiệm nhân vật, thêm cinemachine

---
+++ Assets/Scripts/Thang/MoveController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Nhận đầu vào từ bàn phím
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        // Chuẩn hóa vector để tránh di chuyển nhanh hơn khi đi chéo
+        movement = movement.normalized;
+    }
+
+    void FixedUpdate()
+    {
+        // Di chuyển nhân vật bằng cách thay đổi vận tốc Rigidbody2D
+        rb.velocity = movement * moveSpeed;
+    }
+}
