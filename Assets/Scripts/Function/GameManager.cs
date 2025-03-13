a6099448abb093e8ee607076da53e87013c9333a
TV-Trong <tvt180602@gmail.com>
2025-03-13 22:15:35 +0700

    Tạo GameManager và event khi chuyển scene

---
+++ Assets/Scripts/Function/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    GameState currentState;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+
+        currentState = GameState.MainMenu;
+    }
+
+    private void Start()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (currentState == GameState.Playing)
+        {
+            // Gọi hàm gì đó khi vào scene chơi game
+        }
+        else
+        {
+            // Khi thoát màn chơi thì gọi hàm này
+        }
+    }
+}
+
+public enum GameState
+{
+    MainMenu,
+    Playing
+}
