b5b6d3c7290b2dbec64d5c024d395c36c96290dc
bapcao <caorua04@gmail.com>
2025-04-04 23:40:16 +0700

    Script hệ thống item và chat

---
+++ Assets/Scripts/Thang/Item/ItemBullet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewItemBullet", menuName = "Item System/Bullet Item")]
+public class ItemBullet : ItemBase
+{
+    public WeaponBase weaponType;
+    public int ammoAmount;
+
+    public override void UseItem(PlayerNetworkProperties player, PlayerInventory inventory)
+    {
+        if (weaponType != null && inventory.ammoCount.ContainsKey(weaponType))
+        {
+            inventory.ammoCount[weaponType] += ammoAmount;
+            Debug.Log($"Cộng dồn đạn cho {weaponType.name}: {ammoAmount} đạn");
+        }
+        else
+        {
+            Debug.Log("Không có vũ khí tương ứng để cộng đạn.");
+        }
+    }
+}
---
+++ Assets/Scripts/Thang/Item/ItemHP.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewItemHP", menuName = "Item System/HP Item")]
+public class ItemHP : ItemBase
+{
+    public float healAmount;
+
+    public override void UseItem(PlayerNetworkProperties player, PlayerInventory inventory)
+    {
+        if (player.CurrentHP < player.BaseHP)
+        {
+            player.HealRpc(healAmount);
+            Debug.Log($"Hồi máu: {healAmount}");
+        }
+        else
+        {
+            Debug.Log("HP đầy, không cần hồi máu.");
+        }
+    }
+}
