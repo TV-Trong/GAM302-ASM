4e912880e277656ed5d227b0d49c1217879d76d0
bapcao <caorua04@gmail.com>
2025-04-04 23:52:53 +0700

    Chat

--- Assets/Scripts/Player/PlayerNetworkProperties.cs
+++ Assets/Scripts/Player/PlayerNetworkProperties.cs
@@ -1,4 +1,4 @@
-using Fusion;
+﻿using Fusion;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -53,4 +53,12 @@ public class PlayerNetworkProperties : NetworkBehaviour
             Runner.Despawn(Object);
         }
     }
+
+    // Thang
+    [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
+    public void HealRpc(float healAmount)
+    {
+        CurrentHP = Mathf.Min(BaseHP, CurrentHP + healAmount); // Hồi máu nhưng không vượt quá giới hạn máu tối đa
+    }
+    
 }
